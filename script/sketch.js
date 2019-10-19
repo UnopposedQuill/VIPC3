f1c5230d7c8e4a2dec7155518f887d92490b5714
Daniel Martinez Bonilla <mabo.daniel@gmail.com>
2019-10-18 23:23:04 -0600

    Forma de cambiar entre el archivo del audio y el monitor de sonido de la computadora Pendiente: que al regresar al archivo pueda regresar al momento en que pauso.

--- script/sketch.js
+++ script/sketch.js
@@ -1,5 +1,6 @@
-let volumen, canvas, textCanvas;
-let sound, fft, amplitude;
+let volumen, canvas, textCanvas, sel;
+let sound, fft, amplitude, mix;
+let soundMode = true; //true=>archivo, false=>audio de la computadora/microfono
 
 let fileInput;
 let hmap = [];
@@ -15,12 +16,21 @@ function setup(){
   fileInput = createFileInput(fileHandle);
   /*Observer del volumen*/
   amplitude = new p5.Amplitude();
+  /*Monitor de audio*/
+  mic = new p5.AudioIn();
   /*trasnformada rapida de fourier, para el espectro*/
   fft = new p5.FFT(0,32);
 
-
   canvas = createCanvas(windowWidth,windowHeight,WEBGL);
   volumen = createSlider(0,1,0.5,0.1);
+  sel = createSelect();
+  sel.option("Archivo");
+  mic.getSources(ls=>{ //lista de sources de audio
+    ls.forEach((d,i)=>{ //data index de cada elemento del array
+      sel.option(d.label);
+    });
+  });
+  sel.changed(selChange);
   
   small = min(width,height);
   big = max(width,height);
@@ -40,17 +50,16 @@ function setup(){
 
 function draw(){
   background(0);
-  if(sound.isLoaded()){
+  if(sound.isLoaded() || !soundMode){
     
-    sound.setVolume(volumen.value());
-    //if(frameCount%2==0){
+    if(soundMode) sound.setVolume(volumen.value());
     /*Aca meto el estado actual del espectro a la cola del mapa de altura*/
     let row = hmap.push([])-1;
     let spectrum = fft.analyze(32);
     for(let i=0;i<spectrum.length;++i) hmap[row][i] = spectrum[i];
     /*Elimino la cabeza si ya hay 32 estados*/
     if(hmap.length>=90) hmap.shift();
-//}
+
     noStroke();
  
     /*Lo que hace esto es renderizar la matriz del mapa de altura
@@ -106,6 +115,7 @@ function draw(){
 } 
 
 function toggleSound(){
+  if(!soundMode) return;
   if(sound.isPlaying()){
     sound.pause(); 
   } else {
@@ -127,13 +137,13 @@ function fileHandle(file){
     textCanvas.text('Archivo no cargado\nEspera unos minutos\nO prueba con otro',200,200);
     sound.stop();
     sound = loadSound(file,soundLoaded,soundError);
-    volume.valumen(0.5);
   } else {
     soundError();
   }
 }
 
 function soundLoaded(){
+  if(!soundMode) return;
   sound.playMode('restart');
   sound.loop();
   document.getElementById("seekTime").max = parseInt(sound.duration());
@@ -145,6 +155,7 @@ function soundError(){
 }
 
 function volumenMusica(self){
+  if(!soundMode) return;
   var entrada = parseInt(self.value);
   console.log(volumen);
   console.log(entrada/10);
@@ -159,6 +170,7 @@ function volumenMusica(self){
 
 
 function tiempoMusica(valor){
+  if(!soundMode) return;
   var minutos = parseInt(valor/60);
   var segundos= parseInt(valor%60);
   if(minutos<=9){
@@ -175,6 +187,7 @@ function tiempoMusica(valor){
 }
 
 function inversorMusical(){
+  if(!soundMode) return;
   if(invertidor){
     sound.rate(1);
     invertidor=false;
@@ -203,10 +216,11 @@ function valorTiempoBarraMusica(valor){
 }
 
 function tiempoBarraMusica(self){
- var tiempo=parseInt(self.value);
- //sound.stop();
- sound.jump(tiempo);
- barraDuracionActiva = true;
+  if(!soundMode) return;
+  var tiempo=parseInt(self.value);
+  //sound.stop();
+  sound.jump(tiempo);
+  barraDuracionActiva = true;
 }
 
 function noActualizarBarraMusica(){
@@ -219,11 +233,12 @@ function ActualSizarBarraMusica(){
 }
 
 function btnStop(){
+  if(!soundMode) return;
   sound.stop();
 }
 
 function actualizarbtn(){
-  
+  if(!soundMode) return; 
   if(sound.isPlaying()){
     document.getElementById("btnPausa").innerHTML = "Play";
     document.getElementById("btnPausa").style.background = "#77FF55";
@@ -252,6 +267,7 @@ function actualizarbtn(){
 }
 
 function Velocidad(self){
+  if(!soundMode) return;
   var entrada = parseInt(self.value);
   if(invertidor){
     entrada=entrada*-1;
@@ -263,6 +279,7 @@ function Velocidad(self){
 }
 
 function btnLoopMetodo(){
+  if(!soundMode) return;
   if(loopActivo){
     loopActivo=false;
   }
@@ -272,6 +289,7 @@ function btnLoopMetodo(){
 }
 
 function noEjecutarLoop(){
+  if(!soundMode) return;
   var test1 = parseInt(sound.currentTime());
   var test2= parseInt(sound.duration());
   //console.log(test1 + " - " + test2);  
@@ -281,4 +299,29 @@ function noEjecutarLoop(){
 
 }
 
-
+function selChange(){
+  let value = sel.value();
+  if(value==="Archivo"){
+    if(!soundMode){ //!sM => estaba con el mic
+      mic.stop();
+      if(sound.isLoaded())sound.play();
+      fft.setInput(sound);
+    }
+    soundMode = true;
+  } else {
+    if(soundMode) sound.stop(); //sM => esataba con sound
+    mic.getSources(ls=>{
+      ls.forEach((d,i)=>{
+        if(d.label === value){
+          mic.setSource(i);
+          return;
+        }
+      });
+    });
+    if(soundMode) { //sM => archivo, hay que arrancar
+      mic.start();
+      fft.setInput(mic);
+    }
+    soundMode = false;
+  }
+}
