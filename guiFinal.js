fe7d2c70ea9e2784167034719486e510c317d112
SSDesth <carlosmontt1993@gmail.com>
2019-10-30 19:33:57 -0600

    cambios menores en e nuevo UI
    se cambiaron algunos tamaños y simbolos en el GUI

--- guiFinal.js
+++ guiFinal.js
@@ -6,16 +6,16 @@ function ocultarSuperior(){
 	
 	if(superiorOculta){
 		componente.style.visibility='visible';
-		componente.style.height="110px";
-		document.getElementById("btnSuperior").innerHTML = "^";
-		document.getElementById("contenedorCanvas").style.height= "calc(100% - 155px)";	
-		document.getElementById("divicion3").style.height= "calc(100% - 155px)";
-		document.getElementById("divicion4").style.height= "calc(100% - 155px)";	
+		componente.style.height="75px";
+		document.getElementById("btnSuperior").innerHTML = "▲";
+		document.getElementById("contenedorCanvas").style.height= "calc(100% - 110px)";	
+		document.getElementById("divicion3").style.height= "calc(100% - 110px)";
+		document.getElementById("divicion4").style.height= "calc(100% - 110px)";	
 	}
 	else{
 		componente.style.visibility='hidden';
 		componente.style.height="0px";
-		document.getElementById("btnSuperior").innerHTML = "v";
+		document.getElementById("btnSuperior").innerHTML = "▼";
 		document.getElementById("contenedorCanvas").style.height= "calc(100% - 40px)";
 		document.getElementById("divicion3").style.height= "calc(100% - 40px)";
 		document.getElementById("divicion4").style.height= "calc(100% - 40px)";
@@ -29,16 +29,16 @@ function ocultarLateral(){
 	var componente = document.getElementById("divicion4");
 	
 	if(superiorLateral){
-		componente.style.width="calc(20% - 50px)";
+		componente.style.width="calc(20% - 44px)";
 		componente.style.visibility='visible';
-		document.getElementById("btnLateral").innerHTML = ">";
-		document.getElementById("contenedorCanvas").style.width= "calc(83% - 45px)";		
+		document.getElementById("btnLateral").innerHTML = "►";
+		document.getElementById("contenedorCanvas").style.width= "calc(80% - 6px)";		
 	}
 	else{
-		componente.style.width="0px";
 		componente.style.visibility='hidden';
-		document.getElementById("btnLateral").innerHTML = "<";
-		document.getElementById("contenedorCanvas").style.width= "calc(100% - 53px)";
+		componente.style.width="0px";
+		document.getElementById("btnLateral").innerHTML = "◄";
+		document.getElementById("contenedorCanvas").style.width= "calc(100% - 55px)";
 	} 	
 
 	superiorLateral=!superiorLateral;
