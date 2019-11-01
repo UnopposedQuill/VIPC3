6eaed857d071cfb29deeef4edae9764cc1cc400b
SSDesth <Carlosmontt19@hotmail.com>
2019-11-01 14:57:25 -0600

    Cambios mayores nuevo GUI

--- guiFinal.js
+++ guiFinal.js
@@ -13,12 +13,16 @@ function ocultarSuperior(){
 		document.getElementById("divicion4").style.height= "calc(100% - 110px)";	
 	}
 	else{
+
+		
 		componente.style.visibility='hidden';
 		componente.style.height="0px";
 		document.getElementById("btnSuperior").innerHTML = "▼";
 		document.getElementById("contenedorCanvas").style.height= "calc(100% - 40px)";
 		document.getElementById("divicion3").style.height= "calc(100% - 40px)";
 		document.getElementById("divicion4").style.height= "calc(100% - 40px)";
+
+
 	} 	
 
 	superiorOculta=!superiorOculta;
@@ -29,10 +33,10 @@ function ocultarLateral(){
 	var componente = document.getElementById("divicion4");
 	
 	if(superiorLateral){
-		componente.style.width="calc(20% - 44px)";
+		componente.style.width="calc(30% - 44px)";
 		componente.style.visibility='visible';
 		document.getElementById("btnLateral").innerHTML = "►";
-		document.getElementById("contenedorCanvas").style.width= "calc(80% - 6px)";		
+		document.getElementById("contenedorCanvas").style.width= "calc(70% - 6px)";		
 	}
 	else{
 		componente.style.visibility='hidden';
